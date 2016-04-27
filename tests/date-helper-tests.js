e38d1010e9ef7c8d2f6ea9c2cc349cf62d538604
Kristijan <kristijan.pajtasev@hotmail.com>
2016-04-28 00:22:42 +0100

    test added

--- tests/date-helper-tests.js
+++ tests/date-helper-tests.js
@@ -3,5 +3,12 @@ describe("Date helper ", function() {
     it("will return corect formated date", function() {
         var date = new Date(2016,1,2);
         expect(dateHelper(date).getFormated("DD/MM/YYYY")).toBe("02/02/2016");
+    });
+
+    it("will set correct month", function() {
+        var date = new Date(2016,1,2);
+        date = dateHelper(date);
+        date.setMonth(3);
+        expect(date.getFormated("DD/MM/YYYY")).toBe("02/03/2016");
     })
 })
