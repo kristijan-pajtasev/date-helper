f2e6b83c8649fd211b8b1271f3547c469073f480
Kristijan <kristijan.pajtasev@hotmail.com>
2016-05-06 20:41:36 +0100

    test added

---
+++ test/date-parser-test.js
@@ -0,0 +1,11 @@
+describe("Date helper ", function() {
+    var assert = require('chai').assert;
+    var dateParser = require("../dev/dateParser");
+
+    it("will return corect date", function() {
+        var date = dateParser.parseToDate("12/02/2016", "DD/MM/YYYY")
+        assert.equal(date.getDate(), 12);
+        assert.equal(date.getMonth(), 1);
+        assert.equal(date.getFullYear(), 2016);
+    });
+});
