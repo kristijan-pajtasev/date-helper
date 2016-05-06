ab0193705f3523cf2d672745ea40e6960b356dd6
Kristijan <kristijan.pajtasev@hotmail.com>
2016-05-06 20:19:36 +0100

    tests changed to chai tests

--- test/date-helper-tests.js
+++ test/date-helper-tests.js
@@ -1,14 +1,16 @@
 describe("Date helper ", function() {
+    var assert = require('chai').assert;
+    var dateHelper = require("../dev/dateHelper");
 
     it("will return corect formated date", function() {
         var date = new Date(2016,1,2);
-        expect(dateHelper(date).getFormated("DD/MM/YYYY")).toBe("02/02/2016");
+        assert.equal(dateHelper(date).getFormated("DD/MM/YYYY"), "02/02/2016");
     });
 
     it("will set correct month", function() {
         var date = new Date(2016,1,2);
         date = dateHelper(date);
         date.setMonth(3);
-        expect(date.getFormated("DD/MM/YYYY")).toBe("02/03/2016");
+        assert.equal(date.getFormated("DD/MM/YYYY"), "02/03/2016");
     })
-})
+});
